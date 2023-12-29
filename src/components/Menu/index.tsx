f6a32d172cb52944d173f5495121da2b336e204d
Sahilbalhra <sahilbalhra0@gmail.com>
2023-12-29 22:30:20 +0530

    Added communication between toolbox and menu via redux toolkit

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,24 +6,63 @@ import {
   faRotateRight,
   faFileArrowDown,
 } from "@fortawesome/free-solid-svg-icons";
+import cx from "classnames";
 import styles from "./index.module.css";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { MENU_ITEMS } from "@/constants";
+import {
+  menuItemClick,
+  actionItemClick,
+  activeMenuItemState,
+} from "@/store/menu/menuSlice";
 
 const Menu = () => {
+  const dispatch = useAppDispatch();
+  const activeMenuItem = useAppSelector(activeMenuItemState);
+
+  const handleMenuClick = (itemName: string) => {
+    dispatch(menuItemClick(itemName));
+  };
   return (
     <div className={styles.menuContainer}>
-      <div className={styles.iconWrapper}>
+      <div
+        className={cx(styles.iconWrapper, {
+          [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL,
+        })}
+        onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}
+      >
         <FontAwesomeIcon icon={faPencil} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div
+        className={cx(styles.iconWrapper, {
+          [styles.active]: activeMenuItem === MENU_ITEMS.ERASER,
+        })}
+        onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}
+      >
         <FontAwesomeIcon icon={faEraser} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div
+        className={cx(styles.iconWrapper, {
+          [styles.active]: activeMenuItem === MENU_ITEMS.UNDO,
+        })}
+        onClick={() => handleMenuClick(MENU_ITEMS.UNDO)}
+      >
         <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div
+        className={cx(styles.iconWrapper, {
+          [styles.active]: activeMenuItem === MENU_ITEMS.REDO,
+        })}
+        onClick={() => handleMenuClick(MENU_ITEMS.REDO)}
+      >
         <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div
+        className={cx(styles.iconWrapper, {
+          [styles.active]: activeMenuItem === MENU_ITEMS.DOWNLOAD,
+        })}
+        onClick={() => handleMenuClick(MENU_ITEMS.DOWNLOAD)}
+      >
         <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
       </div>
     </div>
---
+++ src/store/menu/menuSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { MENU_ITEMS } from "@/constants";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../store";
+
+interface menuState {
+  activeMenuItem: string;
+  actionMenuItem: string | null;
+}
+
+const initialState: menuState = {
+  activeMenuItem: MENU_ITEMS.PENCIL,
+  actionMenuItem: null,
+};
+
+export const menuSlice = createSlice({
+  name: "menu",
+  initialState: initialState,
+  reducers: {
+    menuItemClick: (state, action: PayloadAction<string>) => {
+      state.activeMenuItem = action.payload;
+    },
+    actionItemClick: (state, action: PayloadAction<string>) => {
+      state.actionMenuItem = action.payload;
+    },
+  },
+});
+
+export const { menuItemClick, actionItemClick } = menuSlice.actions;
+
+export const activeMenuItemState = (state: RootState) => state.MenuReducer.activeMenuItem;
+
+export default menuSlice.reducer;
