a4ed8fec4180a6f3d81b5631271da68b2ef7656f
Sahilbalhra <sahilbalhra0@gmail.com>
2024-01-01 15:12:43 +0530

    env for production

--- src/socket.ts
+++ src/socket.ts
@@ -1,2 +1,4 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:4000/");
+const URL =
+  process.env.NODE_ENV === "production" ? "https://scratch-book-server-lb69.onrender.com/" : "http://localhost:4000/";
+export const socket = io(URL);
